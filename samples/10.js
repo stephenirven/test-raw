b5023eb1ebd47f15cd026bf16a9e0df7a19f3e07
Steve <stephen.irven@gmail.com>
2025-05-28 15:30:20 +0100

    Create 10.js

---
+++ samples/10.js
@@ -0,0 +1,16 @@
+console.log("nothing to see here");
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+const max = 5;
+
+const n = new Node(5);
+let current = n;
+
+for(let i = 0;i < 5; i++){
+  current.next = new Node(i);
+  current = current.next;
+}
