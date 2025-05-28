8be7a3d8235895644e0b6b5c0e1c1bee3c9aafa0
Steve <stephen.irven@gmail.com>
2025-05-28 11:33:49 +0100

    Create 7.js

---
+++ samples/7.js
@@ -0,0 +1,17 @@
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const l = new Node(1);
+const n = new Node(10);
+
+console.table(n);
+console.log(n);
+console.log(JSON.stringify(n));
+console.log(Object.id(n));
+for(let key of Object.keys(n)){
+  console.log(key + " - " + n[key]);
+}
