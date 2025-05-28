a8a328dd47371804ed91ddd9963fc8d93c4aff63
Steve <stephen.irven@gmail.com>
2025-05-28 11:25:15 +0100

    Create 4.js

---
+++ samples/4.js
@@ -0,0 +1,16 @@
+class Node(val){
+  constructor(){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const n = new Node(10);
+
+console.table(n);
+console.log(n);
+console.log(JSON.stringigy(n));
+console.log(Object.id(n));
+for(let key of Object.keys(n)){
+  console.log(key + " - " + n[key];
+}
