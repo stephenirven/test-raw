08230a27db9cf913d61b48363674780684cd034e
Steve <stephen.irven@gmail.com>
2025-05-30 17:47:50 +0100

    Update map.js

--- samples/map.js
+++ samples/map.js
@@ -1,9 +1,13 @@
-const m = new Map();
-
-
-m.set(10, "ten");
-m.set(20, "twenty");
-
-console.log(m.has(10));
-
-console.log(map.size)
+  wrapper = function keys(obj) {
+    throwIfNullUndefined(obj);
+    if (obj instanceof Interpreter.Object) {
+      obj = obj.properties;
+    }
+    return thisInterpreter.nativeToPseudo(Object.keys(obj));
+  };
+  this.setProperty(
+    this.OBJECT,
+    "keys",
+    this.createNativeFunction(wrapper, false),
+    Interpreter.NONENUMERABLE_DESCRIPTOR
+  );
