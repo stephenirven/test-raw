ea2995b2d46dfeb96409b8bf5dc8d48a4e5b1101
Steve <stephen.irven@gmail.com>
2025-05-30 16:15:23 +0100

    Create mapset.js

---
+++ samples/mapset.js
@@ -0,0 +1,16 @@
+const s = new Set();
+
+console.log(s.has(10));
+
+s.add(10);
+
+console.log(s.has(10));
+
+const m = new Map();
+
+m.set(1, "hello");
+m.set(2, "goodbye");
+m.set(4, "this");
+m.set(5, "is");
+m.set(9, "some");
+m.set(20, "data");
