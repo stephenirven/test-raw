5851094882ca2219a6e94a3e44b42811768631d1
Steve <stephen.irven@gmail.com>
2025-05-31 10:53:04 +0100

    Create builtin.js

---
+++ samples/builtin.js
@@ -0,0 +1,103 @@
+
+// AggregateError
+
+// Array
+
+const array =  [1,2,3];
+
+// ArrayBuffer
+
+const arrayBuffer = new ArrayBuffer()
+// AsyncFunction
+// AsyncGenerator
+// AsyncGeneratorFunction
+// AsyncIterator
+// Atomics
+
+// BigInt
+
+const bigint = BigInt(9007199254740991);
+
+// BigInt64Array
+
+const bigint64Array = new BigInt64Array(buffer);
+bigint64Array[0] = 5886014448488689n;
+bigint64Array[1] = 1881938909131133n;
+bigint64Array[2] = 1898875537769492n;
+
+// BigUint64Array
+
+const biguint64Array = new BigUint64Array(2);
+biguint64Array[0] = 42n;
+
+// Boolean
+
+const bool= Boolean(true);
+
+// DataView
+// Date
+
+const date = new Date("December 17, 1995 03:24:00"); // DISCOURAGED: may not work in all runtimes
+
+// decodeURI()
+// decodeURIComponent()
+// encodeURI()
+// encodeURIComponent()
+// Error
+
+
+// escape()Deprecated
+// eval()
+// EvalError
+// FinalizationRegistry
+// Float16Array
+// Float32Array
+// Float64Array
+// Function
+// Generator
+// GeneratorFunction
+// globalThis
+// Infinity
+// Int16Array
+// Int32Array
+// Int8Array
+// InternalErrorNon-standard
+// Intl
+// isFinite()
+// isNaN()
+// Iterator
+// JSON
+// Map
+// Math
+// NaN
+// Number
+// Object
+// parseFloat()
+// parseInt()
+// Promise
+// Proxy
+// RangeError
+// ReferenceError
+// Reflect
+// RegExp
+
+const re = /bar/g;
+
+// Set
+// SharedArrayBuffer
+// String
+// Symbol
+// SyntaxError
+// TemporalExperimental
+// TypedArray
+// TypeError
+// Uint16Array
+// Uint32Array
+// Uint8Array
+// Uint8ClampedArray
+// undefined
+// unescape()Deprecated
+// URIError
+// WeakMap
+// WeakRef
+// WeakSet
