7c11bd77a30f456824ef72ebfd197d3275ae827a
Steve <stephen.irven@gmail.com>
2025-06-04 20:30:48 +0100

    Create list-reverse2.js

---
+++ samples/list-reverse2.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const n = new Node(1);
+n.next = new Node(2);
+n.next.next = new Node(3)
+n.next.next.next = new Node(4);
+
+let current = n;
+let prev = null;
+
+while (current != null) {
+  const next = current.next;
+
+  current.next = prev;
+
+  prev = current;
+  current = next;
+}
+
+const newList = prev;
