5b1c45ed1445a6297f253b3677c945ff01160c8a
Steve <stephen.irven@gmail.com>
2025-06-05 13:04:35 +0100

    Create sum-recursive.js

---
+++ samples/js/linked-list/sum-recursive.js
@@ -0,0 +1,29 @@
+/*
+ *  Sum a linked list - Iterative method
+ */
+
+// Class for linked list nodes
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// Set up  linked list
+let a = new Node(1);
+a.next = new Node(2);
+a.next.next = new Node(3);
+a.next.next.next = new Node(4);
+
+
+function sumRecurse(head, val=0) {
+  if (head == null) {
+    return val;
+  }
+  val = val + head.val;
+  head = head.next;
+  return sumRecurse(head, val);
+}
+
+const total = sumRecurse(a)
