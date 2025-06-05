0875949cd77a3bfb2b7a7d236ba572543ebab616
Steve <stephen.irven@gmail.com>
2025-06-05 10:08:08 +0100

    Update list-reverse.js

--- samples/list-reverse.js
+++ samples/list-reverse.js
@@ -1,3 +1,8 @@
+/*
+ *  Reverse a linked list - toy example
+ */
+
+// Class for linked list nodes
 class Node {
   constructor(val) {
     this.val = val;
@@ -5,21 +10,26 @@ class Node {
   }
 }
 
+// quick and dirty setup for a four node linked list
+// 1 -> 2 -> 3 -> 4
 const n = new Node(1);
 n.next = new Node(2);
 n.next.next = new Node(3)
 n.next.next.next = new Node(4);
 
-let current = n;
-let prev = null;
+// Reverse the linked list
+
+let current = n; // the current node to process
+let prev = null; // the previous node
 
 while (current != null) {
-  const next = current.next;
+  const next = current.next; // get a reference to the next node
+  current.next = prev; // set the next to the previous
 
-  current.next = prev;
-
-  prev = current;
-  current = next;
+  prev = current; // update the previous node
+  current = next; // and the next node for the next iteration
 }
 
+// prev now references the reverse list
 const newList = prev;
+// 4 -> 3 -> 2 -> 1
