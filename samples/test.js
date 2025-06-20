4db6649ca3835b9d6a58b6f2db553247c09ff262
Steve <stephen.irven@gmail.com>
2025-06-20 14:48:43 +0100

    Create test.js

---
+++ samples/test.js
@@ -0,0 +1,14 @@
+const s = new String("HELLO");
+
+const d = new Date();
+
+const sd = new Set();
+sd.add(s);
+sd.add(d);
+
+const a = [];
+a.push(1);
+a.push(2);
+a.push("hello");
+a.push(s);
+a.push(d);
