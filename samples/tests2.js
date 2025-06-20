3e3821029ef598f76f31e5d71f5bc7ff5315a305
Steve <stephen.irven@gmail.com>
2025-06-20 18:10:25 +0100

    Create tests2.js

---
+++ samples/tests2.js
@@ -0,0 +1,17 @@
+const s = new String("HELLO");
+
+let s_p1 = 2;
+let s_p2 = 4;
+
+const d = new Date();
+
+const sd = new Set();
+sd.add(s);
+sd.add(d);
+
+const a = [];
+a.push(1);
+a.push(2);
+a.push("hello");
+a.push(s);
+a.push(d);
