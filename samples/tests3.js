3aa6545dfc0b41fbb2881b1799b7fe7b79367ca1
Steve <stephen.irven@gmail.com>
2025-06-21 07:52:37 +0100

    Create tests3.js

---
+++ samples/tests3.js
@@ -0,0 +1,24 @@
+const s = new String("HELLO");
+
+const f = (a,d)=> a + d;
+function repeat(str, num){
+  console.log(str.repeat(num));
+
+}
+
+let s_p1 = 2;
+let s_p2 = 4;
+
+
+const d = new Date();
+
+const sd = new Set();
+sd.add(s);
+sd.add(d);
+
+const a = [];
+a.push(1);
+a.push(2);
+a.push("hello");
+a.push(s);
+a.push(d);
