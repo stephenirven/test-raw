34f9fdb2a951e0c6382fc9389f21f78d84bc0030
Stephen Irven <stephen.irven@gmail.com>
2025-06-27 12:42:05 +0100

    add binary tree sample

---
+++ samples/js/binary-tree/bfs-values.js
@@ -0,0 +1,37 @@
+let a = new BinaryNode("a");
+let b = new BinaryNode("b");
+let c = new BinaryNode("c");
+let d = new BinaryNode("d");
+let e = new BinaryNode("e");
+let f = new BinaryNode("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+function breadthFirstIter(tree) {
+  const queue = [tree];
+  const values = [];
+
+  while (queue.length > 0) {
+    const current = queue.shift();
+    values.push(current.val);
+    if (current.left) queue.push(current.left);
+    if (current.right) queue.push(current.right);
+  }
+  return values;
+}
+
+const i = breadthFirstIter(a);
+
+console.log(i);
+
+export default class BinaryNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
